426229ff955963e22cf9b1491fac7ba4ead63860
wannabeCitizen <doorsofskirpan@gmail.com>
2016-06-28 11:00:12 -0600

    graph selection component in basic form

--- src/js/dataPanel.js
+++ src/js/dataPanel.js
@@ -5,8 +5,6 @@ Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
 Licenses.
 
-You may obtain a copy of the ECL 2.0 License and BSD License at
-https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.txt
 */
 
 (function ($, fluid) {
@@ -64,5 +62,7 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
         }
 
     }
+
+
 
 })(jQuery, fluid, Papa);
--- src/js/graphSelector.js
+++ src/js/graphSelector.js
@@ -5,18 +5,16 @@ Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
 Licenses.
 
-You may obtain a copy of the ECL 2.0 License and BSD License at
-https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.txt
 */
 
 (function ($, fluid) {
 
     "use strict";
 
-    fluid.defaults("floe.dataDashboard.graph", {
+    fluid.defaults("floe.dataDashboard.graphSelector", {
         gradeNames: ["fluid.viewComponent"],
         selectors: {
-            graphSelection: ".floec-graphSelect",
+            graphSelection: ".floec-graphSelecter",
         },
         strings: {
             defaultText: "Please choose a graph type."
@@ -27,7 +25,32 @@ https://raw.githubusercontent.com/fluid-project/chartAuthoring/master/LICENSE.tx
                 // Tells us which graph template we're going to load
             }
         },
+        listeners: {
+            "onCreate.setSelectorOptions" : "floe.dataDashboard.graphSelector.setOptions",
+            "onCreate.bindDOMChange" : "floe.dataDashboard.graphSelector.optionsBinding"
+                
+        }
 
-    })
+    });
+
+    floe.dataDashboard.graphSelector.optionsBinding = function (that) {
+        var selector = that.locate("graphSelection");
+        selector.on("change", function () {
+            that.applier.change("graph", selector.val());
+        });
+    };
+
+    floe.dataDashboard.graphSelector.setOptions = function (that) {
+        var options = [ 
+            { value: "pie", name: "Pie Chart" },
+            { value: "line", name: "Line Graph" }
+        ]
+        var selector = that.locate("graphSelection");
+
+        fluid.each(options, function (ind, val) {
+            var newOption = "<option value=" + val.value + ">" + val.name + "</option>";
+            selector.append(newOption);
+        });
+    };
 
 })(jQuery, fluid);
